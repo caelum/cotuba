fce14cf509d15a7e0c3c58e149ffc5d2b42c76ec
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-14 11:53:51 -0200

    passo 2

--- src/main/java/cotuba/LeitorOpcoesCLI.java
+++ src/main/java/cotuba/LeitorOpcoesCLI.java
@@ -1,8 +1,46 @@
 package cotuba;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class LeitorOpcoesCLI {
 
 	public LeitorOpcoesCLI(String[] args) {
+
+		Options options = new Options();
+
+		Option opcaoDeDiretorioDosMD = new Option("d", "dir", true,
+				"Diretório que contem os arquivos md. Default: diretório atual.");
+		options.addOption(opcaoDeDiretorioDosMD);
+
+		Option opcaoDeFormatoDoEbook = new Option("f", "format", true,
+				"Formato de saída do ebook. Pode ser: pdf ou epub. Default: pdf");
+		options.addOption(opcaoDeFormatoDoEbook);
+
+		Option opcaoDeArquivoDeSaida = new Option("o", "output", true,
+				"Arquivo de saída do ebook. Default: book.{formato}.");
+		options.addOption(opcaoDeArquivoDeSaida);
+
+		Option opcaoModoVerboso = new Option("v", "verbose", false, "Habilita modo verboso.");
+		options.addOption(opcaoModoVerboso);
+
+		CommandLineParser cmdParser = new DefaultParser();
+		HelpFormatter ajuda = new HelpFormatter();
+		CommandLine cmd;
+
+		try {
+			cmd = cmdParser.parse(options, args);
+		} catch (ParseException e) {
+			System.err.println(e.getMessage());
+			ajuda.printHelp("cotuba", options);
+			System.exit(1);
+			return;
+		}
 
 	}
 
--- src/main/java/cotuba/Main.java
+++ src/main/java/cotuba/Main.java
@@ -40,36 +40,6 @@ import nl.siegmann.epublib.service.MediatypeService;
 public class Main {
 
 	public static void main(String[] args) {
-		Options options = new Options();
-
-		Option opcaoDeDiretorioDosMD = new Option("d", "dir", true,
-				"Diretório que contem os arquivos md. Default: diretório atual.");
-		options.addOption(opcaoDeDiretorioDosMD);
-
-		Option opcaoDeFormatoDoEbook = new Option("f", "format", true,
-				"Formato de saída do ebook. Pode ser: pdf ou epub. Default: pdf");
-		options.addOption(opcaoDeFormatoDoEbook);
-
-		Option opcaoDeArquivoDeSaida = new Option("o", "output", true,
-				"Arquivo de saída do ebook. Default: book.{formato}.");
-		options.addOption(opcaoDeArquivoDeSaida);
-
-		Option opcaoModoVerboso = new Option("v", "verbose", false,
-				"Habilita modo verboso.");
-		options.addOption(opcaoModoVerboso);
-		
-		CommandLineParser cmdParser = new DefaultParser();
-		HelpFormatter ajuda = new HelpFormatter();
-		CommandLine cmd;
-
-		try {
-			cmd = cmdParser.parse(options, args);
-		} catch (ParseException e) {
-			System.err.println(e.getMessage());
-			ajuda.printHelp("cotuba", options);
-			System.exit(1);
-			return;
-		}
 
 		Path diretorioDosMD;
 		String formato;
