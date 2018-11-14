6a36b924703af8e6249e26925ef8aba71ce71da7
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-14 12:16:10 -0200

    passo 2

--- src/main/java/cotuba/GeradorEPUB.java
+++ src/main/java/cotuba/GeradorEPUB.java
@@ -1,10 +1,79 @@
 package cotuba;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.util.stream.Stream;
+
+import org.commonmark.node.AbstractVisitor;
+import org.commonmark.node.Heading;
+import org.commonmark.node.Node;
+import org.commonmark.node.Text;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.Resource;
+import nl.siegmann.epublib.epub.EpubWriter;
+import nl.siegmann.epublib.service.MediatypeService;
 
 public class GeradorEPUB {
 
 	public void gera(Path diretorioDosMD, Path arquivoDeSaida) {
+
+		Book epub = new Book();
+
+		PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*.md");
+		try (Stream<Path> arquivosMD = Files.list(diretorioDosMD)) {
+			arquivosMD.filter(matcher::matches).sorted().forEach(arquivoMD -> {
+				Parser parser = Parser.builder().build();
+				Node document = null;
+				try {
+					document = parser.parseReader(Files.newBufferedReader(arquivoMD));
+					document.accept(new AbstractVisitor() {
+						@Override
+						public void visit(Heading heading) {
+							if (heading.getLevel() == 1) {
+								// capítulo
+								String tituloDoCapitulo = ((Text) heading.getFirstChild()).getLiteral();
+								// TODO: usar título do capítulo
+							} else if (heading.getLevel() == 2) {
+								// seção
+							} else if (heading.getLevel() == 3) {
+								// título
+							}
+						}
+
+					});
+				} catch (Exception ex) {
+					throw new RuntimeException("Erro ao fazer parse do arquivo " + arquivoMD, ex);
+				}
+
+				try {
+					HtmlRenderer renderer = HtmlRenderer.builder().build();
+					String html = renderer.render(document);
+
+					// TODO: usar título do capítulo
+					epub.addSection("Capítulo", new Resource(html.getBytes(), MediatypeService.XHTML));
+
+				} catch (Exception ex) {
+					throw new RuntimeException("Erro ao renderizar para HTML o arquivo " + arquivoMD, ex);
+				}
+			});
+		} catch (IOException ex) {
+			throw new RuntimeException("Erro tentando encontrar arquivos .md em " + diretorioDosMD.toAbsolutePath(),
+					ex);
+		}
+
+		EpubWriter epubWriter = new EpubWriter();
+
+		try {
+			epubWriter.write(epub, Files.newOutputStream(arquivoDeSaida));
+		} catch (IOException ex) {
+			throw new RuntimeException("Erro ao criar arquivo EPUB: " + arquivoDeSaida.toAbsolutePath(), ex);
+		}
 
 	}
 
--- src/main/java/cotuba/Main.java
+++ src/main/java/cotuba/Main.java
@@ -37,60 +37,7 @@ public class Main {
 				geradorPDF.gera(diretorioDosMD,	arquivoDeSaida);
 
 			} else if ("epub".equals(formato)) {
-				Book epub = new Book();
 
-				PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*.md");
-				try (Stream<Path> arquivosMD = Files.list(diretorioDosMD)) {
-					arquivosMD
-						.filter(matcher::matches)
-						.sorted()
-						.forEach(arquivoMD -> {
-							Parser parser = Parser.builder().build();
-							Node document = null;
-							try {
-								document = parser.parseReader(Files.newBufferedReader(arquivoMD));
-								document.accept(new AbstractVisitor() {
-									@Override
-									public void visit(Heading heading) {
-										if (heading.getLevel() == 1) {
-											// capítulo
-											String tituloDoCapitulo = ((Text) heading.getFirstChild()).getLiteral();
-											// TODO: usar título do capítulo
-										} else if (heading.getLevel() == 2) {
-											// seção
-										} else if (heading.getLevel() == 3) {
-											// título
-										}
-									}
-
-								});
-							} catch (Exception ex) {
-								throw new RuntimeException("Erro ao fazer parse do arquivo " + arquivoMD, ex);
-							}
-
-							try {
-								HtmlRenderer renderer = HtmlRenderer.builder().build();
-								String html = renderer.render(document);
-
-								// TODO: usar título do capítulo
-								epub.addSection("Capítulo", new Resource(html.getBytes(), MediatypeService.XHTML));
-
-							} catch (Exception ex) {
-								throw new RuntimeException("Erro ao renderizar para HTML o arquivo " + arquivoMD, ex);
-							}
-						});
-				} catch (IOException ex) {
-					throw new RuntimeException(
-							"Erro tentando encontrar arquivos .md em " + diretorioDosMD.toAbsolutePath(), ex);
-				}
-
-				EpubWriter epubWriter = new EpubWriter();
-
-				try {
-					epubWriter.write(epub, Files.newOutputStream(arquivoDeSaida));
-				} catch (IOException ex) {
-					throw new RuntimeException("Erro ao criar arquivo EPUB: " + arquivoDeSaida.toAbsolutePath(), ex);
-				}
 			} else {
 				throw new RuntimeException("Formato do ebook inválido: " + formato);
 			}
