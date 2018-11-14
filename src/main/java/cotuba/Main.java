90b41302c2f2089e4a2cb882b68d88be2ffe4f2b
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-14 12:08:55 -0200

    passo 4

--- src/main/java/cotuba/Main.java
+++ src/main/java/cotuba/Main.java
@@ -5,7 +5,6 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
-import java.util.List;
 import java.util.stream.Stream;
 
 import org.commonmark.node.AbstractVisitor;
@@ -14,15 +13,6 @@ import org.commonmark.node.Node;
 import org.commonmark.node.Text;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
-
-import com.itextpdf.html2pdf.HtmlConverter;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.AreaBreak;
-import com.itextpdf.layout.element.IBlockElement;
-import com.itextpdf.layout.element.IElement;
-import com.itextpdf.layout.property.AreaBreakType;
 
 import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.domain.Resource;
