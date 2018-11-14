5933973ecc8cba5dbe1b61e089577ba7bace35dd
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-14 19:17:52 -0200

    passo 2

--- src/main/java/cotuba/application/Cotuba.java
+++ src/main/java/cotuba/application/Cotuba.java
@@ -24,20 +24,6 @@ public class Cotuba {
 		ebook.setArquivoDeSaida(arquivoDeSaida);
 		ebook.setCapitulos(capitulos);
 
-		GeradorEbook gerador;
-
-		if ("pdf".equals(formato)) {
-
-			gerador = new GeradorPDF();
-
-		} else if ("epub".equals(formato)) {
-
-			gerador = new GeradorEPUB();
-
-		} else {
-			throw new RuntimeException("Formato	do	ebook	inválido:	" + formato);
-		}
-
 		gerador.gera(ebook);
 
 	}
--- src/main/java/cotuba/application/GeradorEbook.java
+++ src/main/java/cotuba/application/GeradorEbook.java
@@ -1,12 +1,31 @@
 package cotuba.application;
 
 import cotuba.domain.Ebook;
+import cotuba.epub.GeradorEPUB;
+import cotuba.pdf.GeradorPDF;
 
 public interface GeradorEbook {
 
 	void gera(Ebook ebook);
 
 	public static GeradorEbook cria(String formato) {
+
+		GeradorEbook gerador;
+
+		if ("pdf".equals(formato)) {
+
+			gerador = new GeradorPDF();
+
+		} else if ("epub".equals(formato)) {
+
+			gerador = new GeradorEPUB();
+
+		} else {
+			throw new RuntimeException("Formato	do	ebook	inválido:	" + formato);
+		}
+
+		return gerador;
+
 	}
 
 }
