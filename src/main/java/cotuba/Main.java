2c91975a30431324a4d90ee9422d86381274e302
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-14 12:08:10 -0200

    passo 2

--- src/main/java/cotuba/Main.java
+++ src/main/java/cotuba/Main.java
@@ -42,63 +42,6 @@ public class Main {
 		try {
 
 			if ("pdf".equals(formato)) {
-				try(PdfWriter writer = new PdfWriter(Files.newOutputStream(arquivoDeSaida));
-					PdfDocument pdf = new PdfDocument(writer);
-					Document pdfDocument = new Document(pdf)) {
-
-					PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*.md");
-					try (Stream<Path> arquivosMD = Files.list(diretorioDosMD)) {
-						arquivosMD
-							.filter(matcher::matches)
-							.sorted()
-							.forEach(arquivoMD -> {
-								Parser parser = Parser.builder().build();
-								Node document = null;
-								try {
-									document = parser.parseReader(Files.newBufferedReader(arquivoMD));
-									document.accept(new AbstractVisitor() {
-										@Override
-										public void visit(Heading heading) {
-											if (heading.getLevel() == 1) {
-												// capítulo
-												String tituloDoCapitulo = ((Text) heading.getFirstChild()).getLiteral();
-												// TODO: usar título do capítulo
-											} else if (heading.getLevel() == 2) {
-												// seção
-											} else if (heading.getLevel() == 3) {
-												// título
-											}
-										}
-
-									});
-								} catch (Exception ex) {
-									throw new RuntimeException("Erro ao fazer parse do arquivo " + arquivoMD, ex);
-								}
-
-								try {
-									HtmlRenderer renderer = HtmlRenderer.builder().build();
-									String html = renderer.render(document);
-
-									List<IElement> convertToElements = HtmlConverter.convertToElements(html);
-									for (IElement element : convertToElements) {
-										pdfDocument.add((IBlockElement) element);
-									}
-									// TODO: não adicionar página depois do último capítulo
-									pdfDocument.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
-
-								} catch (Exception ex) {
-									throw new RuntimeException("Erro ao renderizar para HTML o arquivo " + arquivoMD, ex);
-								}
-
-							});
-					} catch (IOException ex) {
-						throw new RuntimeException(
-								"Erro tentando encontrar arquivos .md em " + diretorioDosMD.toAbsolutePath(), ex);
-					}
-
-				} catch (Exception ex) {
-					throw new RuntimeException("Erro ao criar arquivo PDF: " + arquivoDeSaida.toAbsolutePath(), ex);
-				}
 
 			} else if ("epub".equals(formato)) {
 				Book epub = new Book();
