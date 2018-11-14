f9ce1250c4bc2bacfd06bbc13ae31cbd41bdd8f8
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-14 12:34:05 -0200

    passo 2

---
+++ src/main/java/cotuba/Ebook.java
@@ -0,0 +1,36 @@
+package cotuba;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class Ebook {
+
+	private String formato;
+	private Path arquivoDeSaida;
+	private List<Capitulo> capitulos;
+
+	public String getFormato() {
+		return formato;
+	}
+
+	public void setFormato(String formato) {
+		this.formato = formato;
+	}
+
+	public Path getArquivoDeSaida() {
+		return arquivoDeSaida;
+	}
+
+	public void setArquivoDeSaida(Path arquivoDeSaida) {
+		this.arquivoDeSaida = arquivoDeSaida;
+	}
+
+	public List<Capitulo> getCapitulos() {
+		return capitulos;
+	}
+
+	public void setCapitulos(List<Capitulo> capitulos) {
+		this.capitulos = capitulos;
+	}
+
+}
