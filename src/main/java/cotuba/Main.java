7f2ce37bb0e241c9482f432aac5df66243c91d0f
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-14 12:17:44 -0200

    passo 4

--- src/main/java/cotuba/Main.java
+++ src/main/java/cotuba/Main.java
@@ -1,23 +1,6 @@
 package cotuba;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.PathMatcher;
-import java.util.stream.Stream;
-
-import org.commonmark.node.AbstractVisitor;
-import org.commonmark.node.Heading;
-import org.commonmark.node.Node;
-import org.commonmark.node.Text;
-import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
-
-import nl.siegmann.epublib.domain.Book;
-import nl.siegmann.epublib.domain.Resource;
-import nl.siegmann.epublib.epub.EpubWriter;
-import nl.siegmann.epublib.service.MediatypeService;
 
 public class Main {
 
