cfe150ee514efed1bbee84cf2fc73d88d5482365
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-14 13:29:07 -0200

    passo 3

--- src/main/java/cotuba/Main.java
+++ src/main/java/cotuba/Main.java
@@ -1,41 +1,22 @@
 package cotuba;
 
 import java.nio.file.Path;
-import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		LeitorOpcoesCLI opcoesCLI = new LeitorOpcoesCLI(args);
+
 		Path diretorioDosMD = opcoesCLI.getDiretorioDosMD();
 		String formato = opcoesCLI.getFormato();
 		Path arquivoDeSaida = opcoesCLI.getArquivoDeSaida();
 		boolean modoVerboso = opcoesCLI.isModoVerboso();
 
-		RenderizadorMDParaHTML renderizador = new RenderizadorMDParaHTML();
-		List<Capitulo> capitulos = renderizador.renderiza(diretorioDosMD);
-
-		Ebook ebook = new Ebook();
-		ebook.setFormato(formato);
-		ebook.setArquivoDeSaida(arquivoDeSaida);
-		ebook.setCapitulos(capitulos);
-
 		try {
 
-			if ("pdf".equals(formato)) {
-
-				GeradorPDF geradorPDF = new GeradorPDF();
-				geradorPDF.gera(ebook);
-
-			} else if ("epub".equals(formato)) {
-
-				GeradorEPUB geradorEPUB = new GeradorEPUB();
-				geradorEPUB.gera(ebook);
-
-			} else {
-				throw new RuntimeException("Formato do ebook inválido: " + formato);
-			}
+			Cotuba cotuba = new Cotuba();
+			cotuba.executa(formato, diretorioDosMD, arquivoDeSaida);
 
 			System.out.println("Arquivo gerado com sucesso: " + arquivoDeSaida);
 
