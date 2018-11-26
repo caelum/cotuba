a0b1bcea85f6b85e5c76d9af7d56d418216c4e37
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-26 16:51:46 -0200

    passo 4

---
+++ tema-paradizo/src/main/java/br/com/paradizo/tema/TemaParadizo.java
@@ -0,0 +1,13 @@
+package br.com.paradizo.tema;
+
+import cotuba.plugin.Plugin;
+
+public class TemaParadizo implements Plugin {
+
+	@Override
+	public String cssDoTema() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
