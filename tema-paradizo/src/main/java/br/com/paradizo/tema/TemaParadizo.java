e79f3d3d7aa7b7b8b3797296adc36dc1f2caf6f1
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-26 17:42:30 -0200

    passo 1

--- tema-paradizo/src/main/java/br/com/paradizo/tema/TemaParadizo.java
+++ tema-paradizo/src/main/java/br/com/paradizo/tema/TemaParadizo.java
@@ -1,5 +1,6 @@
 package br.com.paradizo.tema;
 
+import cotuba.domain.Ebook;
 import cotuba.plugin.Plugin;
 
 public class TemaParadizo implements Plugin {
@@ -7,6 +8,11 @@ public class TemaParadizo implements Plugin {
 	@Override
 	public String cssDoTema() {
 		return FileUtils.getResourceContents("/tema.css");
+	}
+
+	@Override
+	public void aposGeracao(Ebook ebook) {
+
 	}
 
 }
