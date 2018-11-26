210837b7ba539ead940f0a5e357697d6e74f3001
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-26 17:51:33 -0200

    passo 4

--- estatisticas-ebook/src/main/java/br/com/cognitio/estatisticas/CalculadoraEstatisticas.java
+++ estatisticas-ebook/src/main/java/br/com/cognitio/estatisticas/CalculadoraEstatisticas.java
@@ -1,5 +1,9 @@
 package br.com.cognitio.estatisticas;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import cotuba.domain.Capitulo;
 import cotuba.domain.Ebook;
 import cotuba.plugin.Plugin;
 
@@ -10,6 +14,16 @@ public class CalculadoraEstatisticas implements Plugin {
 	}
 
 	public void aposGeracao(Ebook ebook) {
+
+		for (Capitulo capitulo : ebook.getCapitulos()) {
+			String html = capitulo.getConteudoHTML();
+			Document doc = Jsoup.parse(html);
+			String textoDoCapitulo = doc.body().text();
+			String[] palavras = textoDoCapitulo.split("\\s+");
+			for (String palavra : palavras) {
+				System.out.println(palavra);
+			}
+		}
 
 	}
 
