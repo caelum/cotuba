43a6fab22d78d37fe8df6f1a13bd1aa998f44e50
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-26 17:02:57 -0200

    passo 1

--- cotuba-cli/src/main/java/cotuba/plugin/Plugin.java
+++ cotuba-cli/src/main/java/cotuba/plugin/Plugin.java
@@ -1,7 +1,15 @@
 package cotuba.plugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface Plugin {
 
 	String cssDoTema();
+
+	static List<String> listaDeTemas() {
+		List<String> temas = new ArrayList<>();
+		return temas;
+	}
 
 }
