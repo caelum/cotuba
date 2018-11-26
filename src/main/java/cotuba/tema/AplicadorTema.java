80d7bf9730c3cdbdabb01732ea73d4f65a99411d
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-26 16:34:19 -0200

    passo 3

--- src/main/java/cotuba/tema/AplicadorTema.java
+++ src/main/java/cotuba/tema/AplicadorTema.java
@@ -1,10 +1,19 @@
 package cotuba.tema;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import cotuba.domain.Capitulo;
 
 public class AplicadorTema {
 
 	public void aplica(Capitulo capitulo) {
+
+		String html = capitulo.getConteudoHTML();
+		Document document = Jsoup.parse(html);
+		String css = "h1 { border-bottom: 1px dashed black; }";
+		document.select("head").append("<style> " + css + " </style>");
+		capitulo.setConteudoHTML(document.html());
 
 	}
 
