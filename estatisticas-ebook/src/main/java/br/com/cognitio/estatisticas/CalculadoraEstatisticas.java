4b613cbfe18905b43c886b6bf9b439cc2ded9b4f
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-26 17:50:28 -0200

    passo 3

---
+++ estatisticas-ebook/src/main/java/br/com/cognitio/estatisticas/CalculadoraEstatisticas.java
@@ -0,0 +1,16 @@
+package br.com.cognitio.estatisticas;
+
+import cotuba.domain.Ebook;
+import cotuba.plugin.Plugin;
+
+public class CalculadoraEstatisticas implements Plugin {
+
+	public String cssDoTema() {
+		return null;
+	}
+
+	public void aposGeracao(Ebook ebook) {
+
+	}
+
+}
