d8560b76d5eb2517107f6011d8b74064319bc9f6
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-11-28 16:56:07 -0200

    passo 1

---
+++ estatisticas-ebook/src/main/java/br/com/cognitio/estatisticas/ContagemPalavras.java
@@ -0,0 +1,12 @@
+package br.com.cognitio.estatisticas;
+
+import java.util.HashMap;
+
+public class ContagemPalavras extends HashMap<String, Integer> {
+
+	private static final long serialVersionUID = 1L;
+
+	public void adicionaPalavra(String palavra) {
+
+	}
+}
