e848958defd7a8da3721de0ff4f407120bb2e5bd
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-04 18:13:00 -0200

    passo 3

--- estatisticas-ebook/src/main/java/br/com/cognitio/estatisticas/ContagemPalavras.java
+++ estatisticas-ebook/src/main/java/br/com/cognitio/estatisticas/ContagemPalavras.java
@@ -48,6 +48,20 @@ public class ContagemPalavras implements Iterable<Contagem> {
 
 	@Override
 	public Iterator<Contagem> iterator() {
-		return null;
+		Iterator<Map.Entry<String, Integer>> iterator = this.map.entrySet().iterator();
+		return new Iterator<ContagemPalavras.Contagem>() {
+			@Override
+			public boolean hasNext() {
+				return iterator.hasNext();
+			}
+
+			@Override
+			public ContagemPalavras.Contagem next() {
+				Map.Entry<String, Integer> entry = iterator.next();
+				String palavra = entry.getKey();
+				int quantidade = entry.getValue();
+				return new ContagemPalavras.Contagem(palavra, quantidade);
+			}
+		};
 	}
 }
