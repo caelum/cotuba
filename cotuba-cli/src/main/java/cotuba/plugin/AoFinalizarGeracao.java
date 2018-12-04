b1e92a5d998865fdac237fca6297643ec44297ab
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-04 16:10:22 -0200

    passo 2

--- cotuba-cli/src/main/java/cotuba/plugin/AoFinalizarGeracao.java
+++ cotuba-cli/src/main/java/cotuba/plugin/AoFinalizarGeracao.java
@@ -1,7 +1,18 @@
 package cotuba.plugin;
 
+import java.util.ServiceLoader;
+
 import cotuba.domain.Ebook;
 
 public interface AoFinalizarGeracao {
+
 	void aposGeracao(Ebook ebook);
+
+	static void gerou(Ebook ebook) {
+		ServiceLoader<AoFinalizarGeracao> loader = ServiceLoader.load(AoFinalizarGeracao.class);
+		for (AoFinalizarGeracao plugin : loader) {
+			plugin.aposGeracao(ebook);
+		}
+	}
+
 }
