b252c5131562065378ae46e8ef51393000ce3faa
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-04 18:20:27 -0200

    passo 1

--- cotuba-cli/src/main/java/cotuba/cli/Main.java
+++ cotuba-cli/src/main/java/cotuba/cli/Main.java
@@ -16,7 +16,7 @@ public class Main {
 		try {
 
 			Cotuba cotuba = new Cotuba();
-			cotuba.executa(opcoesCLI);
+			cotuba.executa(opcoesCLI, System.out::println);
 
 			System.out.println("Arquivo gerado com sucesso: " + arquivoDeSaida);
 
