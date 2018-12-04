dbf3494df0af12826db2885aedaf6f3d729546e1
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-04 16:49:21 -0200

    passo 1

--- cotuba-cli/src/main/java/cotuba/application/Cotuba.java
+++ cotuba-cli/src/main/java/cotuba/application/Cotuba.java
@@ -19,10 +19,7 @@ public class Cotuba {
 		RenderizadorMDParaHTML renderizador = RenderizadorMDParaHTML.cria();
 		List<Capitulo> capitulos = renderizador.renderiza(diretorioDosMD);
 
-		Ebook ebook = new Ebook();
-		ebook.setFormato(formato);
-		ebook.setArquivoDeSaida(arquivoDeSaida);
-		ebook.setCapitulos(capitulos);
+		Ebook ebook = new Ebook(formato, arquivoDeSaida, capitulos);
 
 		GeradorEbook gerador = GeradorEbook.cria(formato);
 		gerador.gera(ebook);
