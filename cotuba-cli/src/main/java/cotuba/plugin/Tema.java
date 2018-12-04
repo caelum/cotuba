a8e05af93458c3e65624e691adce4e6057ab66d4
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-04 15:43:55 -0200

    passo 2

--- cotuba-cli/src/main/java/cotuba/plugin/Tema.java
+++ cotuba-cli/src/main/java/cotuba/plugin/Tema.java
@@ -1,5 +1,19 @@
 package cotuba.plugin;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ServiceLoader;
+
 public interface Tema {
 	String cssDoTema();
+
+	static List<String> listaDeTemas() {
+		List<String> temas = new ArrayList<>();
+		ServiceLoader<Tema> loader = ServiceLoader.load(Tema.class);
+		for (Tema plugin : loader) {
+			String css = plugin.cssDoTema();
+			temas.add(css);
+		}
+		return temas;
+	}
 }
